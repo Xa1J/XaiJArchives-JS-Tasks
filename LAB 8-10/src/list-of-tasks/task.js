6b377cfb2fa95983e6c10692e18f9a158c0eeb4d
Xa1J <146411203+Xa1J@users.noreply.github.com>
2023-12-25 03:02:14 +0300

    Add files via upload

---
+++ LAB 8-10/src/list-of-tasks/task.js
@@ -0,0 +1,62 @@
+export const tasks = [
+  {
+    id: "1",
+    title: "Eat food",
+    status: "backlog",
+  },
+  {
+    id: "2",
+    title: "wash the car",
+    status: "backlog",
+  },
+  {
+    id: "3",
+    title: "cook",
+    status: "backlog",
+  },
+  {
+    id: "4",
+    title: "learn Java",
+    status: "active",
+  },
+  {
+    id: "5",
+    title: "go to school",
+    status: "active",
+  },
+  {
+    id: "6",
+    title: "Do projects",
+    status: "active",
+  },
+  {
+    id: "7",
+    title: "attend a meeting",
+    status: "done",
+  },
+  {
+    id: "8",
+    title: "Go out with friends",
+    status: "done",
+  },
+  {
+    id: "9",
+    title: "Call Mum",
+    status: "done",
+  },
+  {
+    id: "10",
+    title: "Go to church",
+    status: "delete",
+  },
+  {
+    id: "11",
+    title: "Take out the trash",
+    status: "delete",
+  },
+  {
+    id: "12",
+    title: "Delutter the room",
+    status: "delete",
+  },
+];
